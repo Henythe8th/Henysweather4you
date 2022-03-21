1d59171687854567d75caa6884d4ef8ea10b0b21
Henythe8th <h.velascoghs@gmail.com>
2022-03-20 22:44:27 -0500

    trying to update javascript so it shows weather

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-let Weather ={
-    "apiKey": https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+let Weather = {
+    "apiKey": "73df6cc7267674cac9cdc1fff7dde6aa"
+    fetchWeather: function () {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=""+ city" "&appid={API key}"
+        ).then((Response) => Response.json()) 
+    }
+    displayWeather: any () function
 }
